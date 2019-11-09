efd08ee1d58605871ebb8882ed8ad8a6264a9cf7
Abdul <abdul@uitouxsolutions.com>
2019-11-09 17:18:04 +0530

    updates

--- src/models/ServiceItemCategory.php
+++ src/models/ServiceItemCategory.php
@@ -2,6 +2,7 @@
 
 namespace Abs\ServiceInvoicePkg;
 
+use App\Company;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -13,4 +14,40 @@ class ServiceItemCategory extends Model {
 		'updated_by_id',
 		'deleted_by_id',
 	];
+
+	public static function createFromObject($record_data) {
+		$company = Company::where('code', $record_data->company)->first();
+		$admin = $company->admin();
+
+		$errors = [];
+		if (!$company) {
+			$errors[] = 'Invalid Company : ' . $record_data->company;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'name' => $record_data->category_name,
+		]);
+		$record->created_by_id = $admin->id;
+		$record->save();
+		return $record;
+	}
+
+	public static function createFromCollection($records) {
+		foreach ($records as $key => $record_data) {
+			try {
+				if (!$record_data->company) {
+					continue;
+				}
+				$record = self::createFromObject($record_data);
+			} catch (Exception $e) {
+				dd($e);
+			}
+		}
+	}
 }
--- src/models/ServiceItemSubCategory.php
+++ src/models/ServiceItemSubCategory.php
@@ -2,6 +2,7 @@
 
 namespace Abs\ServiceInvoicePkg;
 
+use App\Company;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -13,4 +14,52 @@ class ServiceItemSubCategory extends Model {
 		'updated_by_id',
 		'deleted_by_id',
 	];
+
+	public static function createFromObject($record_data) {
+
+		$errors = [];
+		$company = Company::where('code', $record_data->company)->first();
+		if (!$company) {
+			dump('Invalid Company : ' . $record_data->company);
+			return;
+		}
+
+		$admin = $company->admin();
+		if (!$admin) {
+			dump('Default Admin user not found');
+			return;
+		}
+
+		$category = ServiceItemCategory::where('name', $record_data->category)->where('company_id', $company->id)->first();
+		if (!$category) {
+			$errors[] = 'Invalid category : ' . $record_data->category;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'company_id' => $company->id,
+			'name' => $record_data->sub_category_name,
+		]);
+		$record->category_id = $category->id;
+		$record->created_by_id = $admin->id;
+		$record->save();
+		return $record;
+	}
+
+	public static function createFromCollection($records) {
+		foreach ($records as $key => $record_data) {
+			try {
+				if (!$record_data->company) {
+					continue;
+				}
+				$record = self::createFromObject($record_data);
+			} catch (Exception $e) {
+				dd($e);
+			}
+		}
+	}
 }
