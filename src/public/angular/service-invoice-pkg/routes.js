bcab693c620d84766e89c4707c20b1f72d55a148
Abdul <abdul@uitouxsolutions.com>
2019-11-11 14:46:26 +0530

    Update routes.js

--- src/public/angular/service-invoice-pkg/routes.js
+++ src/public/angular/service-invoice-pkg/routes.js
@@ -2,17 +2,17 @@ app.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider.
     //DN
-    when('/service-invoice-pkg/dn/list', {
-        template: '<service-invoice-list></dn-list>',
+    when('/service-invoice-pkg/service-invoice/list', {
+        template: '<service-invoice-list></service-invoice-list>',
         title: 'Service Invoices',
     }).
-    when('/service-invoice-pkg/dn/add', {
+    when('/service-invoice-pkg/service-invoice/add', {
         template: '<service-invoice-form></service-invoice-form>',
         title: 'Add Service Invoice',
     }).
     when('/service-invoice-pkg/service-invoice/edit/:id', {
         template: '<service-invoice-form></service-invoice-form>',
-        title: 'Edit Debit Note',
+        title: 'Edit Service Invoice',
     })
 
 }]);
