c8bf9db55e3c825ca309f24413a544786ef48ba2
Abdul <abdul@uitouxsolutions.com>
2019-11-12 11:13:26 +0530

    route updates

--- src/public/angular/service-invoice-pkg/routes.js
+++ src/public/angular/service-invoice-pkg/routes.js
@@ -15,4 +15,32 @@ app.config(['$routeProvider', function($routeProvider) {
         title: 'Edit Service Invoice',
     })
 
+    //SERVICE ITEM CATEGORIES
+    when('/service-invoice-pkg/service-item-category/list', {
+        template: '<service-item-category-list></service-item-category-list>',
+        title: 'Service Item Categories',
+    }).
+    when('/service-invoice-pkg/service-item-category/add', {
+        template: '<service-item-category-form></service-item-category-form>',
+        title: 'Add Service Item Category',
+    }).
+    when('/service-invoice-pkg/service-item-category/edit/:id', {
+        template: '<service-item-category-form></service-item-category-form>',
+        title: 'Edit Service Item Category',
+    })
+
+    //SERVICE ITEMS
+    when('/service-invoice-pkg/service-item/list', {
+        template: '<service-item-list></service-item-list>',
+        title: 'Service Items',
+    }).
+    when('/service-invoice-pkg/service-item/add', {
+        template: '<service-item-form></service-item-form>',
+        title: 'Add Service Item',
+    }).
+    when('/service-invoice-pkg/service-item/edit/:id', {
+        template: '<service-item-form></service-item-form>',
+        title: 'Edit Service Item',
+    })
+
 }]);
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,14 @@
 Route::group(['namespace' => 'Abs\ServiceInvoicePkg', 'middleware' => ['web', 'auth'], 'prefix' => 'cn-dn-pkg'], function () {
 	Route::get('/service-invoices/get-list', 'ServiceInvoiceController@getServiceInvoiceList')->name('getServiceInvoiceList');
 	Route::get('/service-invoice/save', 'ServiceInvoiceController@saveServiceInvoice')->name('saveServiceInvoice');
+
+	//SERVICE ITEM CATEGORIES
+	Route::get('/service-item-categories/get-list', 'ServiceItemCategoryController@getServiceItemCategoryList')->name('getServiceItemCategoryList');
+	Route::get('/service-item-category/get-form-data/{id?}', 'ServiceItemCategoryController@getServiceItemCategoryFormData')->name('getServiceItemCategoryFormData');
+	Route::post('/service-item-category/save', 'ServiceItemCategoryController@saveServiceItemCategory')->name('saveServiceItemCategory');
+
+	//SERVICE ITEMS
+	Route::get('/service-items/get-list', 'ServiceItemController@getServiceItemList')->name('getServiceItemList');
+	Route::get('/service-item/get-form-data/{id?}', 'ServiceItemCategoryController@getServiceItemFormData')->name('getServiceItemFormData');
+	Route::post('/service-item/save', 'ServiceItemController@saveServiceItem')->name('saveServiceItem');
 });
