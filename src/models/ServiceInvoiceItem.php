209c9246b121756eedfdd8635abf4cd56d61af70
Abdul <abdul@uitouxsolutions.com>
2019-11-15 17:53:05 +0530

    seeder updates

--- src/models/ServiceInvoiceItem.php
+++ src/models/ServiceInvoiceItem.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Abs\ServiceInvoicePkg;
-
+use App\Company;
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceInvoiceItem extends Model {
@@ -27,5 +27,77 @@ class ServiceInvoiceItem extends Model {
 
 	public function taxes() {
 		return $this->belongsToMany('Abs\TaxPkg\Tax', 'service_invoice_item_tax', 'service_invoice_item_id', 'tax_id')->withPivot(['percentage', 'amount']);
+	}
+
+	public static function createFromCollection($records) {
+		foreach ($records as $key => $record_data) {
+			try {
+				if (!$record_data->company) {
+					continue;
+				}
+				$record = self::createFromObject($record_data);
+			} catch (Exception $e) {
+				dd($e);
+			}
+		}
+	}
+
+	public static function createFromObject($record_data) {
+		$company = Company::where('code', $record_data->company)->first();
+		$admin = $company->admin();
+
+		$errors = [];
+		if (!$company) {
+			$errors[] = 'Invalid Company : ' . $record_data->company;
+		}
+
+		$service_invoice = ServiceInvoice::where('number', $record_data->service_invoice_number)->where('company_id', $company->id)->first();
+		if (!$service_invoice) {
+			$errors[] = 'Invalid service invoice number : ' . $record_data->service_invoice_number;
+		}
+
+		$service_item = ServiceItem::where('code', $record_data->service_item_code)->where('company_id', $company->id)->first();
+		if (!$service_item) {
+			$errors[] = 'Invalid service item code : ' . $record_data->service_item_code;
+		}
+
+		if (count($errors) > 0) {
+			dump($errors);
+			return;
+		}
+
+		$record = self::firstOrNew([
+			'service_invoice_id' => $service_invoice->id,
+			'service_item_id' => $service_item->id,
+		]);
+		$record->description = $record_data->description;
+		$record->qty = $record_data->qty;
+		$record->rate = $record_data->rate;
+		$record->sub_total = $record->qty * $record->rate;
+		$record->save();
+
+		if ($service_invoice->branch->state->id == $service_invoice->customer->primaryAddress->state->id) {
+			$tax_type_id = 1160; //With State
+		} else {
+			$tax_type_id = 1161; //Inter State
+		}
+		$taxes = $service_item->taxCode->taxes;
+		foreach ($taxes as $tax) {
+			if ($tax->type_id == $tax_type_id) {
+				// dump($tax->pivot->percentage);
+				$record->taxes()->syncWithoutDetaching([
+					$tax->id => [
+						'percentage' => $tax->pivot->percentage,
+						'amount' => round($record->sub_total * $tax->pivot->percentage / 100, 2),
+					],
+				]);
+			}
+		}
+
+		$service_invoice->items_count = $service_invoice->serviceInvoiceItems()->count('id');
+		$service_invoice->amount_total = $service_invoice->serviceInvoiceItems()->sum('sub_total');
+		$service_invoice->save();
+
+		return $record;
 	}
 }
