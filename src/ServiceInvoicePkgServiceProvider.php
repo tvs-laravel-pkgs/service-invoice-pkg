5b4f885dc729f404d7904f33cbe7d48d8c36cdd0
Abdul <47030364+kaligan-uitoux@users.noreply.github.com>
2019-11-27 20:48:06 +0530

    Update ServiceInvoicePkgServiceProvider.php

--- src/ServiceInvoicePkgServiceProvider.php
+++ src/ServiceInvoicePkgServiceProvider.php
@@ -14,7 +14,6 @@ class ServiceInvoicePkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/migrations');
-		$this->loadViewsFrom(__DIR__ . '/views', 'basic');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
 		]);
