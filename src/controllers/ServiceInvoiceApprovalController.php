6848f7a7e2266a7134b7a611a67bc75631226373
Parthiban <parthiban@uitouxsolutions.com>
2021-12-11 13:54:54 +0530

    Update ServiceInvoiceApprovalController.php

--- src/controllers/ServiceInvoiceApprovalController.php
+++ src/controllers/ServiceInvoiceApprovalController.php
@@ -715,7 +715,7 @@ class ServiceInvoiceApprovalController extends Controller {
 					$this->data['service_invoice_pdf'] = $service_invoice_save;
 					// dd($this->data['service_invoice_pdf']);
 
-					$tax_list = Tax::where('company_id', 1)->get();
+					$tax_list = Tax::where('company_id', $service_invoice->company_id)->get();
 					$this->data['tax_list'] = $tax_list;
 					// dd($this->data['tax_list']);
 					$path = storage_path('app/public/service-invoice-pdf/');
