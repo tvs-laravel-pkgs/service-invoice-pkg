8ac236138aee91df0781f04b0c79519764694575
karthickn-uitoux <karthick.n@uitoux.in>
2022-02-02 13:17:46 +0530

    config

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'DEV' => env('SERVICE_INVOICE_PKG_DEV', false),
-	"eInvoiceConfigIdCN"=>129203, //129203
-	"eInvoiceConfigIdDN"=>129204, //129204
-	"eInvoiceConfigIdINV"=>129205, //129205
+	"DEV" => env('SERVICE_INVOICE_PKG_DEV', false),
+	"eInvoiceConfigIdCN" => 129222, 
+	"eInvoiceConfigIdDN" => 129223, 
+	"eInvoiceConfigIdINV" => 129224, 
 ];
