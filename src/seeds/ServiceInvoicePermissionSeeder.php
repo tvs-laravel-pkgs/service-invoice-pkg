792338503324eff7d94657097dbd34dace7b1b9b
keerthana-uitoux <keerthana@uitoux.in>
2022-08-25 11:23:56 +0530

    Update ServiceInvoicePermissionSeeder.php

--- src/seeds/ServiceInvoicePermissionSeeder.php
+++ src/seeds/ServiceInvoicePermissionSeeder.php
@@ -221,6 +221,157 @@ class ServiceInvoicePermissionSeeder extends Seeder {
 				'name' => 'without-e-invoice-only',
 				'display_name' => 'Without E-Invoice Only',
 			],
+			//MASTER > SERVICE INVOICE
+			3063 => [
+				'display_order' => 12,
+				'parent_id' => NULL,
+				'name' => 'honda-service-invoices',
+				'display_name' => 'Honda Service Invoices',
+			],
+			3064 => [
+				'display_order' => 1,
+				'parent_id' => 3063,
+				'name' => 'honda-add-service-invoice',
+				'display_name' => 'Add',
+			],
+			3065 => [
+				'display_order' => 2,
+				'parent_id' => 3063,
+				'name' => 'honda-edit-service-invoice',
+				'display_name' => 'Edit',
+			],
+			3066 => [
+				'display_order' => 3,
+				'parent_id' => 3063,
+				'name' => 'honda-delete-service-invoice',
+				'display_name' => 'Delete',
+			],
+
+			3067 => [
+				'display_order' => 3,
+				'parent_id' => 3063,
+				'name' => 'honda-create-service-invoice-for-all-outlets',
+				'display_name' => 'Created for All Outlets',
+			],
+			3068 => [
+				'display_order' => 4,
+				'parent_id' => 3063,
+				'name' => 'honda-create-cn',
+				'display_name' => 'Create CN',
+			],
+			3069 => [
+				'display_order' => 5,
+				'parent_id' => 3063,
+				'name' => 'honda-create-dn',
+				'display_name' => 'Create DN',
+			],
+			3070 => [
+				'display_order' => 6,
+				'parent_id' => 3063,
+				'name' => 'honda-view-all-cn-dn',
+				'display_name' => 'View All',
+			],
+			3071 => [
+				'display_order' => 7,
+				'parent_id' => 3063,
+				'name' => 'honda-view-own-cn-dn',
+				'display_name' => 'View Own Only',
+			],
+			3072 => [
+				'display_order' => 8,
+				'parent_id' => 3063,
+				'name' => 'honda-view-outlet-based-cn-dn',
+				'display_name' => 'View Outlet Based',
+			],
+			3073 => [
+				'display_order' => 9,
+				'parent_id' => 3063,
+				'name' => 'honda-import-cn-dn',
+				'display_name' => 'Import',
+			],
+			3074 => [
+				'display_order' => 10,
+				'parent_id' => 3063,
+				'name' => 'honda-create-inv',
+				'display_name' => 'Create INV',
+			],
+			3075 => [
+				'display_order' => 11,
+				'parent_id' => 3063,
+				'name' => 'honda-view-sbu-based-cn-dn',
+				'display_name' => 'View SBU Based',
+			],
+
+			3076 => [
+				'display_order' => 13,
+				'parent_id' => 840,
+				'name' => 'honda-cndn-reports',
+				'display_name' => 'CNDN Reports',
+			],
+
+			3077 => [
+				'display_order' => 1,
+				'parent_id' => 3076,
+				'name' => 'tcs-export-own',
+				'display_name' => 'TCS Export Own',
+			],
+			3078 => [
+				'display_order' => 2,
+				'parent_id' => 3076,
+				'name' => 'honda-tcs-export-all',
+				'display_name' => 'TCS Export All',
+			],
+			3079 => [
+				'display_order' => 3,
+				'parent_id' => 3076,
+				'name' => 'honda-tcs-export-outlet-based',
+				'display_name' => 'TCS Export outlet-based',
+			],
+
+			3080 => [
+				'display_order' => 4,
+				'parent_id' => 3076,
+				'name' => 'honda-gst-export',
+				'display_name' => 'GST Export',
+			],
+			3081 => [
+				'display_order' => 5,
+				'parent_id' => 3076,
+				'name' => 'honda-cndn-export',
+				'display_name' => 'CNDN Export',
+			],
+
+			3082 => [
+				'display_order' => 18,
+				'parent_id' => 3063,
+				'name' => 'honda-service-invoice-irn-cancel',
+				'display_name' => 'Service Invoice IRN Cancelation',
+			],
+
+			3083 => [
+				'display_order' => 14,
+				'parent_id' => 3063,
+				'name' => 'honda-service-invoice-cancel',
+				'display_name' => 'B2C Service Invoice Cancelation',
+			],
+			3084 => [
+				'display_order' => 15,
+				'parent_id' => 3063,
+				'name' => 'honda-allow-e-invoice-selection',
+				'display_name' => 'Allow E-Invoice Selection',
+			],
+			3085 => [
+				'display_order' => 16,
+				'parent_id' => 3063,
+				'name' => 'honda-e-invoice-only',
+				'display_name' => 'E-Invoice Only',
+			],
+			3086 => [
+				'display_order' => 17,
+				'parent_id' => 3063,
+				'name' => 'honda-without-e-invoice-only',
+				'display_name' => 'Without E-Invoice Only',
+			],
 
 		];
 
