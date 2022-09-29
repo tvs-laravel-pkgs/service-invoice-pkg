224f950161b06780e5a1b155a058d919cc46384a
keerthana-uitoux <keerthana@uitoux.in>
2022-09-29 18:15:45 +0530

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -3,5 +3,8 @@ return [
 	'DEV' => env('SERVICE_INVOICE_PKG_DEV', false),
 	"eInvoiceConfigIdCN" => 129222, 
 	"eInvoiceConfigIdDN" => 129223, 
-	"eInvoiceConfigIdINV" => 129224, 
+	"eInvoiceConfigIdINV" => 129224,
+	"eInvoiceConfigIdHondaCN" => 10057, 
+	"eInvoiceConfigIdHondaDN" => 10058, 
+	"eInvoiceConfigIdHondaINV" => 10059,  
 ];
